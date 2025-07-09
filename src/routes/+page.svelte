2983c31d9216b0e4f6f2c78843e8a83a7040b1fd
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2025-07-09 00:08:52 -0300

    Reemplazar contenido de bienvenida por componentes de portada y cuenta regresiva

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import Contdown from '../components/sections/contdown.svelte';
+  import Fontpage from '../components/sections/fontpage.svelte';
+</script>
+
+<!-- Background -->
+<div class="h-screen w-screen fixed top-0 left-0 z-[-100] overflow-hidden">
+  <img
+    class="h-screen w-screen object-cover absolute top-0 left-0 z-[-100]"
+    src="https://fixdate.io/modelo-invitacion/65/img/fondo.webp"
+  />
+</div>
+
+<Fontpage />
+<Contdown />
+
